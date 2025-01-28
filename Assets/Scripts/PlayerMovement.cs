1d2fa46eb09f669ff3a34efad3ab39b88a9ea3d4
Rasmus Svala <rasmus.svala@hotmail.com>
2025-01-28 23:02:08 +0100

    pong basics

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public string inputAxis = "Vertical";
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // move body
+        float movement = Input.GetAxis(inputAxis);
+        rb.linearVelocity = new Vector2(0, movement * speed);
+
+        // clamp y-axis
+        float clampedY = Mathf.Clamp(transform.position.y, -3f, 3f);
+        transform.position = new Vector2(transform.position.x, clampedY);
+    }
+}
