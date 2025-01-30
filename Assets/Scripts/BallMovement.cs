efedd193f5aff88eb682c0a4afeaa1741a1a6957
Rasmus Svala <rasmus.svala@hotmail.com>
2025-01-30 16:12:01 +0100

    added physics material to ball

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -15,21 +15,4 @@ public class BallMovement : MonoBehaviour
         Vector2 direction = new Vector2(initX, initY).normalized;
         rb.linearVelocity = direction * speed;
     }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        // WIP
-        // if (collision.gameObject.CompareTag("Paddle"))
-        // {
-        //     Vector2 newDirection = Vector2.Reflect(
-        //         rb.linearVelocity,
-        //         collision.GetContact(0).normal
-        //     );
-        //     rb.linearVelocity = newDirection.normalized * speed;
-        // }
-        // else if (collision.gameObject.CompareTag("Wall"))
-        // {
-        //     rb.linearVelocity = Vector2.Reflect(rb.linearVelocity, collision.contacts[0].normal);
-        // }
-    }
 }
